5fbb063632592eeeeb296b4bf09c2da1050b3e9a
Ruby Rana <rrubyrana8@gmail.com>
2021-08-01 17:08:17 +0530

    first commit

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+// making the component(a simple js function returning a JSX
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date}> </ExpenseDate>
+      <div>
+        {" "}
+        <h2 className="expense-item h2"> {props.title} </h2>{" "}
+      </div>
+      <div className="expense-item__price"> {props.amount} </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
